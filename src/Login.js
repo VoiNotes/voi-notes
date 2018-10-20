7c37dfec724353efde7afeb40637384d0431ef78
Devansh Sharma <devansh.sharma@gmail.com>
2018-10-20 18:59:46 +0530

    Login page fixed with local CSS files

--- src/Login.js
+++ src/Login.js
@@ -9,10 +9,14 @@ class Login extends Component {
                     <div className="Login">
                         <header className="Login">
                             <img src="./assets/images/lock.svg"></img>
-                            <p> Login </p>
-                            <form action="#" method="POST">
-                                <input type="text" name="user" />
-                            </form>
+                            <center>
+                                <p> Login </p>
+                                <form action="#" method="POST">
+                                    <input type="text" name="user" placeholder="Username" /><br></br><br></br>
+                                    Meeting Time: <input type="time" name="mtime" min="9:00" max="20:00" /><br></br><br></br>
+                                    <input type="submit" name="submit" value="Start" className="btn btn-success" />
+                                </form>
+                            </center>
                         </header>
                     </div>
                 </div>
